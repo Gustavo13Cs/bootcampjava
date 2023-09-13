922be57c872bee1c0df59bc3b83accd8af128cd7
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2023-09-13 15:37:02 -0300

    .

--- TS/src/index.ts
+++ TS/src/index.ts
@@ -1,12 +1,46 @@
-type heroi = {
-    name: string;
-    vulgo: string;
+// tipos primitivos: boolean, number,string
+let ligado: boolean = false;
+let nome: string = "Gustavo";
+let idade: number = 30;
+
+//tipos especiais 
+//null
+//undefined 
+
+//tipos abrangentes any e void 
+
+
+//objeto - sem previsilibidade
+
+let produto:object = {
+    name: "falipe",
+    cidade: "sp",
+    idade: 30,
 }
-function printaObjeto(pessoa: heroi) {
-    console.log(pessoa);
+
+type ProdutoLoja = {
+    nome: string;
+    preco: number;
+    unidades: number;
 }
-printaObjeto({
-    name: "bruce wayne",
-    vulgo: "batman",
-})
-console.log("Ola mundo");
+
+// arrays
+
+let dados: string[] = ["Gustavo", "Ana", "Felipe"] // ou 
+let dados2: Array<string> = ["Gustavo", "Ana", "Felipe"] 
+
+// VARIOS TIPOS NE UM ARRAY
+let infos: (string | number)[] = ["fELIPE","2"]
+
+// Tuplas tem que seguir a exata ordem
+
+let boleto:[string,number,number] = ["agua conta", 199.9, 19.90]
+
+// array metodos
+
+dados.map, dados.filter,dados.slice //etc isso tem no JS 
+
+//Datas
+
+let aniversario:Date = new Date("2022-12-01 05:00");
+console.log(aniversario.toString());
